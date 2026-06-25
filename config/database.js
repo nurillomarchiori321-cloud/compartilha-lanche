62c50b130d57eebe981c902d57eda6b53a1dc9d3
Murillo Marchiori <nurillo.marchiori321@gmail.com>
2026-06-25 15:26:45 -0300

    fix: volta para sqlite3 com rebuild no deploy

--- config/database.js
+++ config/database.js
@@ -1,11 +1,9 @@
 const { Sequelize } = require('sequelize');
 const path = require('path');
-const BetterSqlite3 = require('better-sqlite3');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: path.join(__dirname, '../database.sqlite'),
-  dialectModule: BetterSqlite3,
   logging: false,
   define: {
     timestamps: true,
